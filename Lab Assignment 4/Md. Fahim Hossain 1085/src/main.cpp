e6209cd93e0472664b5e28fd831953bbda62f553
Fahim Hossain <fahimhossain5485@gmail.com>
2026-04-28 15:54:41 +0600

    Add screenshot

--- Lab Assignment 4/Md. Fahim Hossain 1085/src/main.cpp
+++ Lab Assignment 4/Md. Fahim Hossain 1085/src/main.cpp
@@ -47,7 +47,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "1085", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "0432320005101085", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
